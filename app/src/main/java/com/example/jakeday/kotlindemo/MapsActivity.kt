2cd84fdaa3494fe15a9494fc49f01d8bc7324157
Jake Day <jaked97@gmail.com>
2018-03-05 00:48:54 -0800

    Implement JSON parsing

--- app/src/main/java/com/example/jakeday/kotlindemo/MapsActivity.kt
+++ app/src/main/java/com/example/jakeday/kotlindemo/MapsActivity.kt
@@ -6,13 +6,11 @@ import android.location.Address
 import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
 import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import com.beust.klaxon.Json
-import com.beust.klaxon.JsonObject
-import com.beust.klaxon.Parser
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -26,8 +24,9 @@ import com.google.android.gms.maps.model.MarkerOptions
 import java.io.IOException
 
 import kotlinx.android.synthetic.main.activity_maps.*
-import org.json.JSONObject
 
+import com.google.gson.GsonBuilder
+import kotlin.reflect.full.memberProperties
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     override fun onMarkerClick(p0: Marker?) = false
@@ -45,15 +44,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        buttonSubmit(R.id.submit)
     }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -81,6 +77,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 val currentLatLng = LatLng(location.latitude, location.longitude)
                 placeMarkerOnMap(currentLatLng)
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                buttonSubmit(R.id.submit, currentLatLng)
             }
         }
     }
@@ -96,7 +93,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
     private fun getAddress(latLng: LatLng): List<Address>? {
         val geocoder = Geocoder(this)
-        val addresses: List<Address>?
+        var addresses: List<Address>? = emptyList()
 
         try {
             addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
@@ -107,7 +104,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         return addresses
     }
 
-    private fun getAddressString(addresses: List<Address>): String {
+    private fun getAddressString(addresses: List<Address>?): String {
         var addressText = ""
         val address: Address?
 
@@ -129,21 +126,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         zip.setText(addresses[0].postalCode)
     }
 
-    private fun parseForm(input: String): JsonObject {
-        val parser = Parser()
-        val stringBuilder = StringBuilder(input)
-        val json: JsonObject = parser.parse(stringBuilder) as JsonObject
-        return json
+    private fun parseForm(input: Form): String? {
+        for (prop in Form::class.memberProperties) {
+            if (prop.get(input).toString().isNullOrBlank()) {
+                return null
+            }
+        }
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        return gson.toJson(input)
     }
 
-    private fun buttonSubmit(buttonId: Int) {
+    private fun buttonSubmit(buttonId: Int, currentLatLng: LatLng) {
         val buttonClick = findViewById<Button>(buttonId)
         buttonClick.setOnClickListener {
-            Toast.makeText(this@MapsActivity, "Form submitted.", Toast.LENGTH_SHORT).show()
+            val form = fillForm(getAddress(currentLatLng))
+            val parsed = parseForm(form)
+            var display = ""
+            display = if (parsed.isNullOrBlank()) {
+                "Form not submitted. One or more fields are null or blank."
+            } else {
+                "Form submitted:\n $parsed"
+            }
+            Toast.makeText(this@MapsActivity, display, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun fillForm(address: List<Address>?): Form {
+        val first = findViewById<TextInputEditText>(R.id.first)
+        val last = findViewById<TextInputEditText>(R.id.last)
+        val street = address!![0].subThoroughfare + " " + address[0].thoroughfare
+        val city = address[0].locality
+        val state = address[0].adminArea
+        val zip = address[0].postalCode
+
+        return Form(first.text.toString(), last.text.toString(), street, city, state, zip)
     }
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
+}
+
+class Form constructor(var first: String, var last: String, var street: String, var city: String, var state: String, var zip: String) {
+    override fun toString(): String = "$first $last\n$street\n$city, $state $zip"
 }
